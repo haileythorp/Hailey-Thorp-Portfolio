809601eef5b3b047f2c432d9084983bd6cd7af1f
Peter Gyory <ptg3662@rit.edu>
2016-10-18 07:40:15 -0400

    Finish global nav styles

--- js/slider.js
+++ js/slider.js
@@ -3,4 +3,19 @@ window.onload = function() {
     dots: true,
   });
 
+  window.addEventListener( 'scroll', function (e) {
+    var top  = window.pageYOffset || document.documentElement.scrollTop;
+
+    if ( top >= window.innerHeight - 80 ) {
+      document.querySelector( '.hero-nav' ).className = 'hero-nav sticky is-stuck';
+      document.querySelector( '.hero-nav__image' ).src = './images/BEETLE_LOGO_MEDIUM.png';
+
+    } else {
+
+      document.querySelector( '.hero-nav' ).className = 'hero-nav sticky';
+      document.querySelector( '.hero-nav__image' ).src = './images/BEETLE_LOGO_LIGHT.png';
+    }
+
+  });
+
 };
